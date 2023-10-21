0b0daf83431d75e00ed8fdc8350e6a974f10e9e1
JrmRytn <jraytana@equicom.com>
2023-10-21 15:52:59 +0800

    end of section 13

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { Activity } from '../models/activity';
 import { toast } from 'react-toastify';
 import { router } from '../router/Routes';
 import { store } from '../stores/store';
+import { User, UserFormValues } from '../models/users';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -11,6 +12,14 @@ const sleep = (delay: number) => {
 }
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
+
+const responseBody =<T> (response: AxiosResponse<T>) => response.data;
+
+axios.interceptors.request.use(config => {
+    const token = store.commonStore.token;
+    if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+})
 
 axios.interceptors.response.use(async response => {
 
@@ -51,9 +60,7 @@ axios.interceptors.response.use(async response => {
             break; 
     }
     return Promise.reject(error);
-})
-
-const responseBody =<T> (response: AxiosResponse<T>) => response.data;
+}) 
 
 const requests = {
     get: <T> (url: string) => axios.get<T>(url).then(responseBody),
@@ -70,10 +77,15 @@ const Activities = {
     delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
-
+const Account  = {
+    current: () => requests.get<User>('/account'),
+    login: (user: UserFormValues) => requests.post<User>('/account/login',user),
+    register: (user: UserFormValues) => requests.post<User>('/account/register',user)
+}
 
 
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 export default agent;
